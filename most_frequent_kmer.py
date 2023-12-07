b5cc2487e50570a8d1246b47c2b52f8ec58172eb
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2023-12-06 18:20:37 -0800

    most_frequent_kmer

---
+++ most_frequent_kmer.py
@@ -0,0 +1,24 @@
+def FrequentWords(Text, k):
+    words = []
+    freq = FrequencyMap(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+        else:
+            continue
+    return words
+
+def FrequencyMap(Text, k):
+    freq_dict = {}
+    n = len(Text)
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        if Pattern in freq_dict:
+            freq_dict[Pattern] += 1
+        else:
+            freq_dict[Pattern] = 1
+    return freq_dict
+
+k_mer = FrequentWords( "CGGAGGACTCTAGGTAACGCTTATCAGGTCCATAGGACATTCA", 3)
+print(k_mer)
