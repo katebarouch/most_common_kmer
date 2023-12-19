30305aa2997ffa1bd34917694054d1ff82234e96
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2023-12-19 12:51:19 -0800

    consensus function

--- kmer_motif.py
+++ kmer_motif.py
@@ -16,3 +16,40 @@ def Count(Motifs):
             count[symbol][j] += 1
                 
     return count
+
+# Input:  A list of kmers Motifs
+# Output: the profile matrix of Motifs, as a dictionary of lists.
+def Profile(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    profile = {}
+    
+    for symbol in "ACGT":
+        profile[symbol] = []
+        for j in range(k):
+             profile[symbol].append(0)
+                
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            profile[symbol][j] += 1 / t
+        
+    return profile
+
+# Input:  A set of kmers Motifs
+# Output: A consensus string of Motifs.
+def Consensus(Motifs):
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+        
+    return consensus
